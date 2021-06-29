4143a534be01bed02f885a6ef9fe4e1a93373260
Ted Duan <yzd3008@icloud.com>
2021-06-29 21:12:15 +0800

    Delete useless files

--- payment-service/src/main/java/cn/yzd3008/java/eshop/paymentservice/biz/web/ProductController.java
+++
@@ -1,4 +0,0 @@
-package cn.yzd3008.java.eshop.paymentservice.biz.web;
-
-public class ProductController {
-}
--- shipping-service/src/main/java/cn/yzd3008/java/eshop/shippingservice/biz/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package cn.yzd3008.java.eshop.paymentservice.biz.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ProductService {
-
-
-}
